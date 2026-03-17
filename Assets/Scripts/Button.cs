3ac5ee171927df9c855fd89cc07bfd2e31ed7913
ag-4477 <sakana967346@gmail.com>
2026-03-17 15:17:25 +0900

    fix: ボタンが選択されたときに変数が重複せずに遷移する処理を追加

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,28 +1,47 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
+using System.Linq;
 
 public class ButtonData : MonoBehaviour
 {
     private string buttonName;
     private int id;
-    public string[] buttonTexts = { "なし", "りんご", "みかん", "バナナ", "ぶどう" };
-    
+    public static string[] buttonTexts = { "なし", "りんご", "みかん", "バナナ", "ぶどう" };
     [SerializeField] private TextMeshProUGUI tmpText;
 
     void Start()
     {
-        id = Random.Range(0, buttonTexts.Length);
-        buttonName = buttonTexts[id];
-        UpdateHandleText();
+        UpdateAllButtons();
     }
 
-    public void UpdateHandleText()
+    public void UpdateAllButtons()
     {
-        tmpText.text = buttonName;
+        ButtonData[] allButtons = Object.FindObjectsByType<ButtonData>(FindObjectsSortMode.None);
+        List<int> randomIds = Enumerable.Range(0, buttonTexts.Length).OrderBy(x => System.Guid.NewGuid()).ToList();
+
+        for (int i = 0; i < allButtons.Length; i++)
+        {
+            if (i < randomIds.Count)
+            {
+                allButtons[i].id = randomIds[i];
+                allButtons[i].buttonName = buttonTexts[randomIds[i]];
+                allButtons[i].SetTextDirectly();
+            }
+        }
+    }
+
+    public void SetTextDirectly()
+    {
+        if (tmpText != null)
+        {
+            tmpText.text = buttonName;
+        }
     }
 
     public void OnButtonClick()
     {
         Debug.Log("ID: " + id + " (" + buttonName + ") が押されました");
+        UpdateAllButtons();
     }
 }
