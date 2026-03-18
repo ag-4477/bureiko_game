1008f987d610c24a5997749de69848b2a0d02fa6
Kento Watanabe <Kenthan9@gmail.com>
2026-03-18 13:53:25 +0900

    仮コミット

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -35,6 +35,8 @@ public class ScoreController : MonoBehaviour
             breikoBarScript.UpdateBreikoValue(buttonId);
             buttonControllerScript.NextBreiko(questionId);
             questionId++;
+            int breikoValue = breikoBarScript.bureikoBarValue;
+            Debug.Log(breikoValue);
         }
         else
         {
