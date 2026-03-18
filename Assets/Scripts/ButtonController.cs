3770a97840fcd3ca330e95c3d8e76e01e0c244fa
yorokan <s243151@stn.nagaokaut.ac.jp>
2026-03-18 17:47:38 +0900

    ありがたい言葉を表示する機構作り途中一旦コミット

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -9,6 +9,7 @@ public class ButtonController : MonoBehaviour
     private GameDataWrapper data;
     private GameObject[] buttons;
     private Button[] buttonScripts;
+    private Text[] bossTextScripts;
 
     
     // Start を Awake に変える
@@ -19,6 +20,8 @@ public class ButtonController : MonoBehaviour
         
         // ボタンの取得も Awake でやっておくと安全です
         buttonScripts = Object.FindObjectsByType<Button>(FindObjectsSortMode.None);
+
+        bossTextScripts = Object.FindObjectsByType<Text>(FindObjectsSortMode.None);
     }
 
     void Start()
@@ -48,8 +51,16 @@ public class ButtonController : MonoBehaviour
         int i = 0;
         foreach (Button buttonScript in buttonScripts)
         {
-            buttonScript.UpdateButtonText(currentQuestionId, data.questions[currentQuestionId].questiondata[i].data.text);
+            buttonScript.UpdateButtonText(currentQuestionId, data.questions[currentQuestionId].questionSet.questiondata[i].data.text);
             i++;
+        }
+    }
+
+    public void DisplayBossText(int questionId)
+    {
+        foreach (Text bossTextScript in bossTextScripts)
+        {
+            bossTextScript.UpdateBossText(data.questions[questionId].questionSet.bossText);
         }
     }
 }
---
+++ Assets/Scripts/Text.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Text : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int id;
+    public string bossMessage;
+    [SerializeField] private TextMeshProUGUI tmpText;
+    void Start()
+    {
+        
+    }
+    public void UpdateBossText(string bossText)
+    {
+        bossMessage = bossText;
+        SetBossText();
+        
+    }
+
+    public void SetBossText()
+    {
+        if (tmpText != null)
+        {
+            tmpText.text = bossMessage;
+        }
+    }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
