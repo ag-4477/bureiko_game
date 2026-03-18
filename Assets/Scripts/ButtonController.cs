ab15c525972ad2408a1a1b9f4575e73a8f7ca82f
yorokan <s243151@stn.nagaokaut.ac.jp>
2026-03-18 11:05:58 +0900

    Fix: 各ボタンにidを付与する関数を追加

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -5,32 +5,23 @@ public class ButtonController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is createdd
     // int currentQuestionId;
-    private JsonPathLoader loader;
-
+    public JsonPathLoader loader;
     private GameDataWrapper data;
+    private List<Button> buttons;
+
     
     void Start()
     {
-        
+        loader = gameObject.GetComponent<JsonPathLoader>();
         data = loader.gameData;
-
-        NextBreiko(0);
     }
 
     void NextBreiko(int currentQuestionId)
     {
-        List<Button> buttons = new List<Button>();
-
         for (int i = 0; i < buttons.Count; i++)
         {
             buttons[i].UpdateButtonText(currentQuestionId, data.questions[currentQuestionId].questiondata[i].data.text);
             Debug.Log(buttons[i].buttonName);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
