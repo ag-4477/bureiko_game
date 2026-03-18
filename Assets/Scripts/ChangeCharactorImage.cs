305e8278fe1c95daca184116e7a50fc2d73f6fa3
池田悠希 <youxichitian47@gmail.com>
2026-03-18 15:04:52 +0900

    無礼講BarValueごとに人物の画像を切り替えられるようにしました

--- Assets/Scripts/ChangeCharactorImage.cs
+++ Assets/Scripts/ChangeCharactorImage.cs
@@ -1,27 +1,38 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 
 public class ChangeCharactorImage : MonoBehaviour
 {
+    public SpriteRenderer sr;
+
     private Image img;
     private GameObject obj;
-    bar bureikoBarScript;
+    BreikoBar bureikoBarScript;
 
     void Start()
     {
         img = GameObject.Find("Man").GetComponent<Image>();
     }
 
-    void Change()
+    void Update()
     {
         obj = GameObject.Find("Slider");
-        bureikoBarScript = obj.GetComponent<bar>();
+        bureikoBarScript = obj.GetComponent<BreikoBar>();
 
         if (bureikoBarScript.bureikoBarValue < 40)
         {
-            img.sprite = 
+            img.sprite = Resources.Load<Sprite>("Charactor/senpai/normal");
+        } 
+        else if (bureikoBarScript.bureikoBarValue > 40 && bureikoBarScript.bureikoBarValue < 80)
+        {
+            img.sprite = Resources.Load<Sprite>("Charactor/senpai/angry");
+        }
+        else
+        {
+            img.sprite = Resources.Load<Sprite>("Charactor/senpai/superAngry");
         }
     }
 }
